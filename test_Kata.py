b8101a753940b2234b21d075fe24f478345d11fe
hepam57 <hepam_57@hotmail.com>
2020-03-15 10:39:41 -0500

    Ciclo TDD 2 (Rojo) Crear prueba que falle cuando se recibe cadena vacia para arreglo con tamaño y minimo

--- test_Kata.py
+++ test_Kata.py
@@ -3,7 +3,7 @@ import Kata
 
 class TestArreglos(TestCase):
     def test_get_arreglos(self):
-        self.assertEqual(Kata.Arreglos.GetArreglos(""), [], "Cadena vacía")
+        self.assertEqual(Kata.Arreglos.GetArreglos(""), [None, None], "Cadena vacía")
 
     def test_get_arreglo_con_un_numero(self):
         self.assertEqual(Kata.Arreglos.GetArreglos("1"), [1], "Arreglo con la cantidad de números")
