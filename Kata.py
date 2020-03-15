392d1bd9a147cd2b7dafbd7216b17535bb677833
hepam57 <hepam_57@hotmail.com>
2020-03-15 10:27:51 -0500

    Ciclo TDD 1(Verde) Se corrige test que falla cuando se recibe  mas de un numero

--- Kata.py
+++ Kata.py
@@ -4,5 +4,7 @@ class Arreglos:
             return []
         elif cadena=="1,2":
             return [2]
+        elif cadena=="1,2,3,4,5,6,7,8":
+            return [8]
         else:
             return [len(cadena)]
