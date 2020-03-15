4122b85d80ebc910526ffa304e72709a74c8329e
Juan Sebastian Alvarez Eraso <juan_alvarez_12345@hotmail.com>
2020-03-15 11:37:30 -0500

    Ciclo TDD 4 (Azul) Refactor para cadenas con 2 números que tienen longitud, mínimo, máximo y promedio

--- Kata.py
+++ Kata.py
@@ -8,11 +8,10 @@ class Arreglos:
                 return [8, 1, 8]
             elif cadena == "10,15,20,35,80":
                 return [5, 10, 80]
-            elif cadena == "1,2":
-                return [len(numeros), int(min(numeros)), int(max(numeros)), 1.5]
-            elif cadena == "66,20":
-                return [len(numeros), int(min(numeros)), int(max(numeros)), 43]
-            elif cadena == "15,15":
-                return [len(numeros), int(min(numeros)), int(max(numeros)), 15]
+
+            suma = 0
+            for numero in numeros:
+                suma += int(numero)
+            return [len(numeros), int(min(numeros)), int(max(numeros)), suma/len(numeros)]
         else:
             return [1, int(cadena), int(cadena), int(cadena)]
