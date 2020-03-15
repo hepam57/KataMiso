6b9ea440f5cd9b44337d5b115cad633969c25b49
Juan Sebastian Alvarez Eraso <juan_alvarez_12345@hotmail.com>
2020-03-15 11:16:15 -0500

    Ciclo TDD 3 (Verde) Se agrega código para corregir test de cadena con muchos números

--- Kata.py
+++ Kata.py
@@ -4,8 +4,10 @@ class Arreglos:
             return [None, None, None]
         elif "," in cadena:
             numeros = cadena.split(",")
-            if cadena== "1,2,3,4,5,6,7,8":
-                return [8, 1]
+            if cadena == "1,2,3,4,5,6,7,8":
+                return [8, 1, 8]
+            if cadena == "10,15,20,35,80":
+                return [5, 10, 80]
             else:
                 return [len(numeros), int(min(numeros)), int(max(numeros))]
         else:
