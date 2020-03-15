7ee33b58079530f194fbcbce341677292c57d699
Juan Sebastian Alvarez Eraso <juan_alvarez_12345@hotmail.com>
2020-03-15 10:43:14 -0500

    Ciclo TDD 2 (Rojo) Se actualiza test de cadena con un solo número para obtener arreglo con tamaño y mínimo

--- test_Kata.py
+++ test_Kata.py
@@ -6,7 +6,7 @@ class TestArreglos(TestCase):
         self.assertEqual(Kata.Arreglos.GetArreglos(""), [None, None], "Cadena vacía")
 
     def test_get_arreglo_con_un_numero(self):
-        self.assertEqual(Kata.Arreglos.GetArreglos("1"), [1], "Arreglo con la cantidad de números")
+        self.assertEqual(Kata.Arreglos.GetArreglos("1"), [1, 1], "Arreglo con la cantidad de números")
 
     def test_get_arreglo_con_dos_numeros(self):
         self.assertEqual(Kata.Arreglos.GetArreglos("1,2"), [2], "Arreglo con  2 números")
