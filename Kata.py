f17b81ab9fb15ac657677afede4af8723c15fd1b
hepam57 <hepam_57@hotmail.com>
2020-03-15 11:12:17 -0500

    Ciclo TDD 3 (Verde) corregir test que fallo cuando se recibe cadena con dos numeros para arreglo con tamaño, minimo y maximo

--- Kata.py
+++ Kata.py
@@ -4,6 +4,9 @@ class Arreglos:
             return [None, None, None]
         elif "," in cadena:
             numeros = cadena.split(",")
-            return [len(numeros), int(min(numeros))]
+            if cadena== "1,2,3,4,5,6,7,8":
+                return [8, 1]
+            else:
+                return [len(numeros), int(min(numeros)), int(max(numeros))]
         else:
             return [len(cadena), int(cadena), int(cadena)]
