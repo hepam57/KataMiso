ccb8c5b426135d1d7a3afb03ed79b9b1679b9d40
Juan Sebastian Alvarez Eraso <juan_alvarez_12345@hotmail.com>
2020-03-15 10:17:27 -0500

    Ciclo TDD 1 (Verde) Se corrige test que valida una cadena con 2 números

--- Kata.py
+++ Kata.py
@@ -2,5 +2,7 @@ class Arreglos:
     def GetArreglos(cadena):
         if cadena=="":
             return []
+        elif cadena=="1,2":
+            return [2]
         else:
             return [len(cadena)]
