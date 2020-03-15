6ffc7cc724d4879e494c063d7ca481b99e2c765b
hepam57 <hepam_57@hotmail.com>
2020-03-15 11:40:48 -0500

    Ciclo TDD 4 (Verde) corregir test que fallo cuando recibe cadena con muchos numeros para arreglo con tamaño, minimo, maximo y promedio

--- Kata.py
+++ Kata.py
@@ -4,11 +4,6 @@ class Arreglos:
             return [None, None, None, None]
         elif "," in cadena:
             numeros = cadena.split(",")
-            if cadena == "1,2,3,4,5,6,7,8":
-                return [8, 1, 8]
-            elif cadena == "10,15,20,35,80":
-                return [5, 10, 80]
-
             suma = 0
             for numero in numeros:
                 suma += int(numero)
