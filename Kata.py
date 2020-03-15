644f8cb6bc1173b943b411d6cef4d688fc014a37
Juan Sebastian Alvarez Eraso <juan_alvarez_12345@hotmail.com>
2020-03-15 10:29:26 -0500

    Ciclo TDD 1 (Azul) Se hace un refactor en el código para las cadenas que contienen comas y muchos números

--- Kata.py
+++ Kata.py
@@ -2,9 +2,8 @@ class Arreglos:
     def GetArreglos(cadena):
         if cadena=="":
             return []
-        elif cadena=="1,2":
-            return [2]
-        elif cadena=="1,2,3,4,5,6,7,8":
-            return [8]
+        elif "," in cadena:
+            numeros = cadena.split(",")
+            return [len(numeros)]
         else:
             return [len(cadena)]
