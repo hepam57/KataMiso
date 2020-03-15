7c0951e678274632746e465dcb51a19f8a07c129
hepam57 <hepam_57@hotmail.com>
2020-03-15 10:03:14 -0500

    Ciclo TDD 1(Rojo) Se agrega test que falla cuando no se recibe un arreglo con 2 numeros

--- test_Kata.py
+++ test_Kata.py
@@ -7,3 +7,6 @@ class TestArreglos(TestCase):
 
     def test_get_arreglo_con_un_numero(self):
         self.assertEqual(Kata.Arreglos.GetArreglos("1"), [1], "Arreglo con la cantidad de números")
+
+    def test_get_arreglo_con_dos_numeros(self):
+        self.assertEqual(Kata.Arreglos.GetArreglos("1,2"), [2], "Arreglo con  2 números")
