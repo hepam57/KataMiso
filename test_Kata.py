68897d7c1ecdd027fae7aca788ef1b95b5ba25e5
Juan Sebastian Alvarez Eraso <juan_alvarez_12345@hotmail.com>
2020-03-15 09:49:45 -0500

    Ciclo TDD 1 (Rojo) Se agrega test que valida el arreglo con la cantidad de números en la cadena

--- test_Kata.py
+++ test_Kata.py
@@ -4,3 +4,6 @@ import Kata
 class TestArreglos(TestCase):
     def test_get_arreglos(self):
         self.assertEqual(Kata.Arreglos.GetArreglos(""), [], "Cadena vacía")
+
+    def test_get_arreglo_con_un_numero(self):
+        self.assertEqual(Kata.Arreglos.GetArreglos("1"), [1], "Arreglo con la cantidad de números")
